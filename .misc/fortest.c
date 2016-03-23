2399c41ddfce121472c42fec3448ebde58f1aed7
Foo174 <withee@gmail.com>
2016-03-22 21:44:47 -0700

    add data types prog

---
+++ .misc/fortest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int i, a;
+   a = 0;
+
+   printf("a=%d\n", a);
+   printf("a=%d\n", a++);
+   printf("a=%d\n", a);
+
+
+   /*for (i = 0; i < 5; ++i)*/
+   return 0;
+}
