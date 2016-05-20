9594a6c40e51ae6e2cdf50dab99f6289dfce9a00
Foo174 <withee@gmail.com>
2016-05-20 11:27:08 -0700

    add ex_03-01 and tweak intro text

---
+++ chapter_03/ex_03-01.c
@@ -0,0 +1,31 @@
+/****************************************************
+* Exercise 03.01 from the K&R book.
+* by Foo74 -- May 20, 2016
+*****************************************************/
+
+/* Include our main header file.*/
+#include "ex_03-01.h"
+
+/* The main function of our program. */
+int main()
+{
+   printf("\n---------------------------------------------------------");
+   printf("\n- Exercise 03.01 from, The C Programming Language,      -");
+   printf("\n- by Brian W. Kernighan and Dennis M. Ritchie, 2nd Ed., -");
+   printf("\n- AT&T Bell Laboratories (Prentice Hall - 1988).        -");   
+   printf("\n---------------------------------------------------------\n\n");
+
+   /* Be a good main function and return 0 because all went fine. */
+   return 0;
+}
+
+
+/* Our functions are defined below here. We could also put them 
+ * in their own files and link them together or #include them.
+ */
+
+/* See header ex_03-01.h for description. */
+int binsearch(int x, int v[], int n)
+{
+   return 0;
+}
---
+++ chapter_03/ex_03-01.h
@@ -0,0 +1,23 @@
+/****************************************************
+* Exercise 03.01 from the K&R book.
+* by Foo74 -- May 20, 2016
+*
+* A program that... 
+*****************************************************/
+
+/* Include the standard io library and standard ints.*/
+#include <stdio.h>
+/*
+#include "../kenlib/print.h"
+*/
+
+/* Define the vars we will use to test so we can
+ * easily change them. Maybe want to have as input in future
+ * or args to command line.
+#define X_BITS 0b11010100
+ */
+
+/* Function that takes in a char, and if it is upper case, converts
+ * it to lower case. If it is already lower then just returns it.
+ */
+int binsearch(int, int [], int);
