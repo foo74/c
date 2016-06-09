a232b33ad902830241eb07206bd9ac5ebeb18a46
root <root@debian.none.com>
2016-06-09 13:25:24 -0700

    working on ex 3-3

--- chapter_03/ex_03-03.c
+++ chapter_03/ex_03-03.c
@@ -9,24 +9,26 @@
 /* The main function of our program. */
 int main()
 {
-   char init_string[MAX] = "This is a string	with some 	tabs	and some \nnewline\n.";
-   char result_string[MAX] = "";
+   char input_str[] = "a-z";
+   char output_str[MAX];
 
    /* Print the header info. */
    printf("\n---------------------------------------------------------");
-   printf("\n- Exercise 03.02 from, The C Programming Language,      -");
+   printf("\n- Exercise 03.03 from, The C Programming Language,      -");
    printf("\n- by Brian W. Kernighan and Dennis M. Ritchie, 2nd Ed., -");
    printf("\n- AT&T Bell Laboratories (Prentice Hall - 1988).        -");   
    printf("\n---------------------------------------------------------\n\n");
 
    /* Print the info about this program. */
-   printf("This program takes a string and converts the tabs and\n");
-   printf("newline to escape sequences \\t and \\n.\n\n");
-   printf("Starting String is:\n%s\n\n", init_string);
+   printf("This program takes a string that contains\n");
+   printf("shorthand and expands it. The function that\n");
+   printf("demonstrates this is expand(). The function\n");
+   printf("allows for letters of either case and digits\n");
+   printf("and handles cases like a-b-c, a-z0-9, and -a-z.\n\n");
 
-   escape(init_string, result_string);
-
-   printf("Processed String is:\n%s\n\n", result_string);
+   printf("Input String is:\n%s\n\n", input_str);
+   expand(input_str, output_str);
+   printf("Processed String is:\n%s\n\n", output_str);
 
    /* Be a good main function and return 0 because all went fine. */
    return 0;
@@ -38,34 +40,32 @@ int main()
  */
 
 /* See header ex_03-03.h for description. */
-void escape(char s[], char t[])
+void expand(char s1[], char s2[])
 {
-   /* i is indexer, and dest_index to keep track of dest array. */
    int i;
-   int dest_index;
-   i = 0;
-   dest_index = 0;
+   int j;
+   int amount;
+   int place;
+   int character;
 
-   /* For each char in the array, while we are not yet at 0. */
-   for (i=0; s[i] != 0 && i < MAX; i++)
+   for (i=0; i<MAX && s1[i] != 0; i++)
    {
-      /* Test the char. */
-      switch (s[i])
+      if (s1[i] == '-')
       {
-         /* The ascii value 9 is a tab. */
-         case 9:
-            t[dest_index++] = '\\';
-            t[dest_index++] = 't';
-            break;
-         /* The ascii value 10 is a newline. */
-         case 10:
-            t[dest_index++] = '\\';
-            t[dest_index++] = 'n';
-            break;
-         /* If not a tab or newline then just copy char. */
-         default:
-            t[dest_index++] = s[i];
-            break;
+         character = s1[i-1];
+         s2[i] = 'x';
+         amount = s1[i+1]-s1[i-1];
+         for (j=i; j<amount; j++)
+            s2[j] = character+j;
+
+         place = j;
+      }
+      else
+      {
+         s2[place] = s1[i];
+         place++;
       }
    }
+
+   s2[place] = 0;
 }
--- chapter_03/ex_03-03.h
+++ chapter_03/ex_03-03.h
@@ -2,7 +2,12 @@
  * Exercise 03.03 from the K&R book.
  * by Foo74 -- June 08, 2016
  *
- * A program that ...
+ * A program that shows the expand() function which
+ * that expands shorthand notation like a-z in the
+ * string s1 into the equivalent complete list
+ * abc...xyz in s2. The function allows for letters
+ * of either case and digits and handles cases
+ * like a-b-c, a-z0-9 and -a-z.
  *****************************************************/
 
 /* Include the standard io library.*/
@@ -14,7 +19,7 @@
 #define MAX 1024
 
 /* Function that ...
- * s:    the source string.
- * t:    the dest string.
+ * s1:    the source string.
+ * t2:    the dest string.
  */
-void escape(char s[], char t[]);
+void expand(char s1[], char s2[]);
