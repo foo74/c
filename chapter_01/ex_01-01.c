ad44afe6b02e46a113671940b2d5306611271a40
Ubuntu <foo1@foo.svubehqnvh2ufelimavpn040ra.dx.internal.cloudapp.net>
2016-11-02 20:39:23 +0000

    ex 1-1 and 1-2

---
+++ chapter_01/ex_01-01.c
@@ -0,0 +1,19 @@
+/****************************************************
+* Exercise 01.01 from the K&R book.
+* by Foo74 -- Date November, 2016
+*****************************************************/
+
+/* Include the standard io library and  constant definitions. */
+#include <stdio.h>
+
+
+/* The main function of our program. */
+int main(int argc, char *argv[])
+{
+   printf("hello, ");
+   printf("world");
+   printf("\n");
+
+   /* Be a good main function and return 0 because all went fine. */
+   return 0;
+}
